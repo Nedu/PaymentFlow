532a90f31aac44a7eb5c3313004a0ab6f0915577
Nedu Robert <nedumrobert@gmail.com>
2018-03-31 19:28:26 +0100

    initialized server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const stripe = require('stripe')('sk_test_Xh65Kx3U5l4YwWgN3Ha9sXnP');
+const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
