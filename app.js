9665805213bee3624d752f02bee1f14d218c37e3
Nedu Robert <nedumrobert@gmail.com>
2018-03-31 21:10:56 +0100

    created charge route to communicate with stripe and charge customers

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 /* Middlewares */
 
 // Handlebars Middleware
-app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.engine('handlebars', expressHandlebars({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
 // BodyParser Middleware
@@ -21,6 +21,24 @@ app.use(express.static(`${__dirname}/public`));
 // Index Route
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+// Charge Route
+app.post('/charge', (req, res) => {
+    const amount = 2500;
+
+    stripe.customers.create({ 
+        email: req.body.stripeEmail, 
+        source: req.body.stripeToken 
+    })
+    .then(customer => stripe.charges.create({
+        amount,
+        description: 'Adventure Corp Product',
+        currency: 'usd',
+        customer: customer.id,
+        receipt_email: customer.email
+    }))
+    .then(charge => res.render('success'));
 });
 
 const port = process.env.PORT || 5000;
